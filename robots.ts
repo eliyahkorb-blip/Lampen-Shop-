de6e12522e33b5b9e98f41c02953e910682a36fb
EKY Media <eliyah.korb@gmail.com>
2026-04-29 00:17:19 +0200

    Delete robots.ts

--- robots.ts
+++
@@ -1,9 +0,0 @@
-import type { MetadataRoute } from "next";
-import { shopConfig } from "@/lib/shop-config";
-
-export default function robots(): MetadataRoute.Robots {
-  return {
-    rules: { userAgent: "*", allow: "/" },
-    sitemap: `${shopConfig.siteUrl}/sitemap.xml`,
-  };
-}
