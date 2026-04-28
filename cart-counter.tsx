923ff4a998b1dc71b2999d14c9f2d8600c98bfc2
EKY Media <eliyah.korb@gmail.com>
2026-04-29 00:15:47 +0200

    Delete cart-counter.tsx

--- cart-counter.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { useCart } from "./cart-provider";
-
-export function CartCounter() {
-  const { count } = useCart();
-  return <span className="cart-count" aria-label={`${count} Artikel im Warenkorb`}>{count}</span>;
-}
